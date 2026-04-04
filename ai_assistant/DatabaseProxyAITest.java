0ec6c66927113d24ee15ff2d49753d2b059f97be
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-04 18:42:42 -0400

    Update DatabaseProxyAITest.java

--- ai_assistant/DatabaseProxyAITest.java
+++ ai_assistant/DatabaseProxyAITest.java
@@ -1,1 +1,59 @@
+package ai_assistant;
 
+import main.models.*;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseProxyAITest {
+
+    private DatabaseProxy proxy;
+
+    @BeforeEach
+    void setup() {
+        proxy = new DatabaseProxy(CSVDatabaseManager.getInstance());
+    }
+
+    @Test
+    void testConstructorNullThrows() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new DatabaseProxy(null);
+        });
+    }
+
+    @Test
+    void testReadUsersDelegation() {
+        assertNotNull(proxy.readUsers());
+    }
+
+    @Test
+    void testWriteUsersDelegation() {
+        proxy.writeUsers(new ArrayList<>());
+        assertEquals(0, proxy.readUsers().size());
+    }
+
+    @Test
+    void testReadEquipmentDelegation() {
+        assertNotNull(proxy.readEquipment());
+    }
+
+    @Test
+    void testWriteEquipmentDelegation() {
+        proxy.writeEquipment(new ArrayList<>());
+        assertEquals(0, proxy.readEquipment().size());
+    }
+
+    @Test
+    void testReservationsDelegation() {
+        proxy.writeReservations(new ArrayList<>());
+        assertEquals(0, proxy.readReservations().size());
+    }
+
+    @Test
+    void testPaymentsDelegation() {
+        proxy.writePayments(new ArrayList<>());
+        assertEquals(0, proxy.readPayments().size());
+    }
+}
