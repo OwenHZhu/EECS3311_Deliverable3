fa7b5b193d8ad8095e68c91f04865bdeee8d0941
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-04 19:42:46 -0400

    Update CSVDatabaseManagerAITest.java

--- ai_assistant/CSVDatabaseManagerAITest.java
+++ ai_assistant/CSVDatabaseManagerAITest.java
@@ -2,6 +2,9 @@ package ai_assistant;
 
 import main.models.*;
 import org.junit.jupiter.api.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.*;
 
