f9921e66042b14cd99d005f51ff0bf7ba4168e56
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 14:48:55 -0400

    Update SensorRandoopTest.java

--- randooptest/SensorRandoopTest.java
+++ randooptest/SensorRandoopTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class SensorRandoopTest {
+public class RegressionTest1 {
 
     public static boolean debug = false;
 
