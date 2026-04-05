e3aa2a26a1ae66486b06aa24b78bfa082c146355
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 14:38:02 -0400

    Update EquipmentRandoopTest.java

--- randooptest/EquipmentRandoopTest.java
+++ randooptest/EquipmentRandoopTest.java
@@ -1,11 +1,11 @@
-package test.java.randoop;
+package randooptest;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class RegressionTest0 {
+public class EquipmentRandoopTest {
 
     public static boolean debug = false;
 
