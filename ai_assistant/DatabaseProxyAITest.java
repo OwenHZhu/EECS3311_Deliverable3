8f271047a8a6ff517f10c02cb56b7d0f9da3493f
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 01:23:45 -0400

    Update DatabaseProxyAITest.java

--- ai_assistant/DatabaseProxyAITest.java
+++ ai_assistant/DatabaseProxyAITest.java
@@ -10,7 +10,7 @@ public class DatabaseProxyAITest {
 
     private DatabaseProxy proxy;
 
-    @BeforeEach
+    @Before
     public void setup() {
         proxy = new DatabaseProxy(CSVDatabaseManager.getInstance());
     }
