cec73dd67cc171d5700d54a778bc48c516b64ce3
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 14:39:19 -0400

    Update SystemAlertRandoopTest.java

--- randooptest/SystemAlertRandoopTest.java
+++ randooptest/SystemAlertRandoopTest.java
@@ -1,11 +1,11 @@
-package test.java.randoop;
+package randooptest;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class RegressionTest4 {
+public class SystemAlertRandoopTest {
 
     public static boolean debug = false;
 
