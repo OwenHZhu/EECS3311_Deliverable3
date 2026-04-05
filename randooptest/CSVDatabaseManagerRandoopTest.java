fee51ead231481e043b12b4bb080ac1d3876cc63
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 14:44:00 -0400

    Update CSVDatabaseManagerRandoopTest.java

--- randooptest/CSVDatabaseManagerRandoopTest.java
+++ randooptest/CSVDatabaseManagerRandoopTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class CSVDatabaseManagerRandoopTest {
+public class RegressionTest2 {
 
     public static boolean debug = false;
 
