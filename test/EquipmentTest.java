68e012796cebd2dd2d24e63e88143a560bf5373a
Sukhpreet Sehjal <sukhs@my.yorku.ca>
2026-04-05 01:29:40 -0400

    Update EquipmentTest.java

--- test/EquipmentTest.java
+++ test/EquipmentTest.java
@@ -2,10 +2,7 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
